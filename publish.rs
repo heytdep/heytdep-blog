ba9cf590d0814a106f4fc9197cfc72ae2928fcc7
Tommaso De Ponti <tommasodeponti@zohomail.eu>
2023-09-02 22:47:25 +0200

    updates and zig soroban sdk post

--- publish.rs
+++ publish.rs
@@ -3,14 +3,70 @@ use std::fs::File;
 use std::io::Write;
 use std::process::Command;
 use std::str;
+use std::path::PathBuf;
 
-fn get_drafts() -> fs::ReadDir {
-    let paths = fs::read_dir("./drafts").unwrap();
+fn extract_name_from_filename(filename: &str) -> &str {
+    // Split the filename by "--" and take the second part
+    let parts: Vec<&str> = filename.split("--").collect();
+    
+    parts[1]
+}
+
+fn extract_numeric_prefix(filename: &str) -> Option<i32> {
+    let parts: Vec<&str> = filename.split("--").collect();
+    if let Some(first_part) = parts.get(0) {
+        let numeric_part = first_part.trim_matches(|c: char| !c.is_numeric());
+        numeric_part.parse::<i32>().ok()
+    } else {
+        None
+    }
+}
+
+fn get_drafts() -> Vec<PathBuf> {
+    let mut paths = fs::read_dir("./drafts")
+        .expect("Failed to read directory")
+        .map(|entry| entry.expect("Failed to read entry").path())
+        .collect::<Vec<PathBuf>>();
+
+    // Sort the paths based on the numeric prefix in the filenames
+    paths.sort_by(|a, b| {
+        let a_filename = a.file_name().unwrap().to_str().unwrap();
+        let b_filename = b.file_name().unwrap().to_str().unwrap();
+
+        if let (Some(a_num), Some(b_num)) =
+            (extract_numeric_prefix(a_filename), extract_numeric_prefix(b_filename))
+        {
+            a_num.cmp(&b_num)
+        } else {
+            // Fallback to comparing full filenames if numeric prefix extraction fails
+            a_filename.cmp(b_filename)
+        }
+    });
+
     paths
 }
 
-fn get_comp() -> fs::ReadDir {
-    let paths = fs::read_dir("./comp_posts").unwrap();
+fn get_comp() -> Vec<PathBuf> {
+    let mut paths = fs::read_dir("./comp_posts")
+        .expect("Failed to read directory")
+        .map(|entry| entry.expect("Failed to read entry").path())
+        .collect::<Vec<PathBuf>>();
+
+    
+    paths.sort_by(|a, b| {
+        let a_filename = a.file_name().unwrap().to_str().unwrap();
+        let b_filename = b.file_name().unwrap().to_str().unwrap();
+
+        if let (Some(a_num), Some(b_num)) =
+            (extract_numeric_prefix(a_filename), extract_numeric_prefix(b_filename))
+        {
+            a_num.cmp(&b_num)
+        } else {
+            // Fallback to comparing full filenames if numeric prefix extraction fails
+            a_filename.cmp(b_filename)
+        }
+    });
+
     paths
 }
 
@@ -31,117 +87,130 @@ fn preemptive_remove() {
 
 fn build_handler(name: String) {
     let splitted: &Vec<&str> = &name.split(".").collect();
-    let dir_name = splitted[0];
-    let post_name_split: &Vec<&str> = &dir_name.split(")--").collect();
-    let mut parent = format!(
-        r#"
-<!DOCTYPE html>
-<html class="">
-<head>
-<link href="../../index.css" rel="stylesheet">
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.6.0/styles/github-dark.min.css">
-<script src="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.6.0/highlight.min.js"></script>
-<!-- Cloudflare Web Analytics --><script defer src='https://static.cloudflareinsights.com/beacon.min.js' data-cf-beacon='{{"token": "e9adb517193447a3a9c4d5064ffa2550"}}'></script><!-- End Cloudflare Web Analytics -->
-<!-- and it's easy to individually load additional languages -->
-<script src="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.6.0/languages/rust.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.6.0/languages/javascript.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.6.0/languages/python.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.6.0/languages/bash.min.js"></script>
-<script>
-fetch("./index.html").then(resp => resp.text().then(content => {{
-    const post = document.createElement("div");
-    post.innerHTML =content;
-    document.getElementById("post").appendChild(post);
-    hljs.highlightAll();
-}}));
-/*
-function resizeIframe(obj) {{
-    obj.style.height = obj.contentWindow.document.documentElement.scrollHeight + 'px';
-    //obj.contentWindow.document.body.style.color = "white"
-    for (el of obj.contentWindow.document.getElementsByTagName("a")) {{
-        el.style.color = "\#337AB7"
+
+    if splitted.len() != 1 {
+        let dir_name = splitted[0];
+        let post_name_split: &Vec<&str> = &dir_name.split(")--").collect();
+        let mut parent = format!(
+            r#"
+    <!DOCTYPE html>
+    <html class="">
+    <head>
+    <link href="../../index.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.6.0/styles/github-dark.min.css">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.6.0/highlight.min.js"></script>
+    <!-- Cloudflare Web Analytics --><script defer src='https://static.cloudflareinsights.com/beacon.min.js' data-cf-beacon='{{"token": "e9adb517193447a3a9c4d5064ffa2550"}}'></script><!-- End Cloudflare Web Analytics -->
+    <!-- and it's easy to individually load additional languages -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.6.0/languages/rust.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.6.0/languages/javascript.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.6.0/languages/python.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.6.0/languages/bash.min.js"></script>
+    <script>
+    fetch("./index.html").then(resp => resp.text().then(content => {{
+        const post = document.createElement("div");
+        post.innerHTML =content;
+        document.getElementById("post").appendChild(post);
+        hljs.highlightAll();
+    }}));
+    /*
+    function resizeIframe(obj) {{
+        obj.style.height = obj.contentWindow.document.documentElement.scrollHeight + 'px';
+        //obj.contentWindow.document.body.style.color = "white"
+        for (el of obj.contentWindow.document.getElementsByTagName("a")) {{
+            el.style.color = "\#337AB7"
+        }}
     }}
-  }}
-*/
-</script>
-</head>
-<body>
-<div id="post">
-<h4><a href="/">see all posts</a></h1>
-<h1 class="title">{}</h1>
-<!--<iframe src="./index.html" frameborder="0" scrolling="no" width="100%" onload="resizeIframe(this)" />-->
-</div>
-     <script>
-function resizeIframe(obj) {{
-    obj.style.height = obj.contentWindow.document.documentElement.scrollHeight + 'px';
-    obj.style.color = "\#fff"
-  }}
+    */
+    </script>
+    </head>
+    <body>
+    <div id="post">
+    <h4><a href="/">see all posts</a></h1>
+    <h1 class="title">{}</h1>
+    <!--<iframe src="./index.html" frameborder="0" scrolling="no" width="100%" onload="resizeIframe(this)" />-->
+    </div>
+        <script>
+    function resizeIframe(obj) {{
+        obj.style.height = obj.contentWindow.document.documentElement.scrollHeight + 'px';
+        obj.style.color = "\#fff"
+    }}
 
-/*fetch("./index.html").then(resp => resp.text().then(content => {{
-const post = document.createElement("div");
-post.innerHTML = /<BODY.*?>([\s\S]*)<\/BODY>/.exec(content)[1];
-document.body.appendChild(post);
-}}));
-*/
+    /*fetch("./index.html").then(resp => resp.text().then(content => {{
+    const post = document.createElement("div");
+    post.innerHTML = /<BODY.*?>([\s\S]*)<\/BODY>/.exec(content)[1];
+    document.body.appendChild(post);
+    }}));
+    */
 
-</script>
-</body>
-</html>
-"#,
-        post_name_split[2].replace("-", " ")
-    );
+    </script>
+    </body>
+    </html>
+    "#,
+            post_name_split[2].replace("-", " ")
+        );
 
-    let mut file = File::create(format!("./comp_posts/{}/post.html", &dir_name)).unwrap();
-    file.write_all(parent.as_bytes());
+        let mut file = File::create(format!("./comp_posts/{}/post.html", &dir_name)).unwrap();
+        file.write_all(parent.as_bytes());
+    }
 }
 
 fn to_html_pd(name: String) {
     let splitted: &Vec<&str> = &name.split(".").collect();
-    let dir_name = splitted[0];
-    let ext_name = splitted[1];
-    let mut child1 = Command::new("mkdir")
-        .arg(format!("./comp_posts/{}", &dir_name))
-        .spawn()
-        .expect("failed to compile");
-    let _result1 = child1.wait().unwrap();
 
-    if ext_name == "tex" {
-        let mut child = Command::new("pandoc")
-            .arg("-f")
-            .arg("latex")
-            .arg("-t")
-            .arg("html")
-            .arg(format!("./drafts/{}", name))
-            .arg("-o")
-            .arg(format!("./comp_posts/{}/index.html", &dir_name))
-            .output()
+    if splitted.len() != 1 {
+        let dir_name = splitted[0];
+        let ext_name = splitted[1];
+        let mut child1 = Command::new("mkdir")
+            .arg(format!("./comp_posts/{}", &dir_name))
+            .spawn()
             .expect("failed to compile");
-        //    let result = child.wait().unwrap();
-        //        let mut out = String::new();
-        //        out.push_str(match str::from_utf8(&child.stdout) {
-        //            Ok(val) => val,
-        //            Err(_) => panic!("got non UTF-8 data from git"),
-        //        });
+        let _result1 = child1.wait().unwrap();
+
+        if ext_name == "tex" {
+            let mut child = Command::new("pandoc")
+                .arg("-f")
+                .arg("latex")
+                .arg("-t")
+                .arg("html")
+                .arg(format!("./drafts/{}", name))
+                .arg("-o")
+                .arg(format!("./comp_posts/{}/index.html", &dir_name))
+                .output()
+                .expect("failed to compile");
+            //    let result = child.wait().unwrap();
+            //        let mut out = String::new();
+            //        out.push_str(match str::from_utf8(&child.stdout) {
+            //            Ok(val) => val,
+            //            Err(_) => panic!("got non UTF-8 data from git"),
+            //        });
+        } else {
+            let mut child = Command::new("pandoc")
+                .arg("-f")
+                .arg("markdown")
+                .arg("-t")
+                .arg("html")
+                .arg(format!("./drafts/{}", name))
+                .arg("-o")
+                .arg(format!("./comp_posts/{}/index.html", &dir_name))
+                .output()
+                .expect("failed to compile");
+            //    let result = child.wait().unwrap();
+            //        let mut out = String::new();
+            //        out.push_str(match str::from_utf8(&child.stdout) {
+            //            Ok(val) => val,
+            //            Err(_) => panic!("got non UTF-8 data from git"),
+            //        });
+        }
+        build_handler(name);
     } else {
-        let mut child = Command::new("pandoc")
-            .arg("-f")
-            .arg("markdown")
-            .arg("-t")
-            .arg("html")
-            .arg(format!("./drafts/{}", name))
-            .arg("-o")
-            .arg(format!("./comp_posts/{}/index.html", &dir_name))
-            .output()
+        let mut child1 = Command::new("mkdir")
+            .arg(format!("./comp_posts/{}", &name))
+            .spawn()
             .expect("failed to compile");
-        //    let result = child.wait().unwrap();
-        //        let mut out = String::new();
-        //        out.push_str(match str::from_utf8(&child.stdout) {
-        //            Ok(val) => val,
-        //            Err(_) => panic!("got non UTF-8 data from git"),
-        //        });
+        let _result1 = child1.wait().unwrap();
     }
-    build_handler(name);
 
+    
     //    fs::write(format!("./comp_posts/{}/index.html", &dir_name), out);
 }
 
@@ -181,29 +250,40 @@ fn gen_index_content(mut posts: Vec<String>) -> String {
     );
 
     let mut li_list = String::new();
-
-    posts.sort();
-    let rev_posts: Vec<String> = posts.into_iter().rev().collect();
-
-    for post in rev_posts {
+    let mut posts: Vec<String> = posts.into_iter().rev().collect();
+    for post in posts {
         let post_name_split: &Vec<&str> = &post.split(")--").collect();
-        let mut dis_name = String::new();
+        
+        if post_name_split.len() != 1 {
+            let mut dis_name = String::new();
 
-        if &post_name_split[1] == &"pub" {
-            dis_name.push_str("&#128215 ");
+            if &post_name_split[1] == &"pub" {
+                dis_name.push_str("&#128215 ");
+            } else {
+                dis_name.push_str("&#128216 ");
+            }
+
+            dis_name.push_str(&post_name_split[2].replace("-", " "));
+            li_list.push_str(&format!(
+                r#"
+    <li>
+    <h3><a href="./comp_posts/{}/post.html">{}</a></h3>
+    </li>
+    "#,
+                &post, &dis_name
+            ))
         } else {
-            dis_name.push_str("&#128216 ");
+            li_list.push_str(&format!(
+                r#"
+    <li>
+    
+    <h3 class="year"><strong>{}</strong></h3>
+    
+    </li>
+    "#,
+                &post.split("--").collect::<Vec<&str>>()[1]
+            ))
         }
-
-        dis_name.push_str(&post_name_split[2].replace("-", " "));
-        li_list.push_str(&format!(
-            r#"
-<li>
-<h3><a href="./comp_posts/{}/post.html">{}</a></h3>
-</li>
-"#,
-            &post, &dis_name
-        ))
     }
 
     let mut ul = String::from(
@@ -211,24 +291,36 @@ fn gen_index_content(mut posts: Vec<String>) -> String {
 <body>
 <div id="content">
 <div class="heading">
-<h1 class="title">tdep's website</h1>
+
+<h3>Tommaso De Ponti (@heytdep)</h3>
 
 <div class="description">
-<p>Hi I'm Tommaso, I am a developer building interesting stuff on the Stellar Network at <a target="_blank" href="https://github.com/xycloo/">Xycloo</a>, and currently part-time contractor helping to build the <a target="_blank" href="https://quest.stellar.org/">Stellar Soroban Quest</a>. My current focus lies in Decentralized ledger technology, smart contracts, Decentralized Finance, and SaaS. Articles with the &#128215 prefix are intended to be read by a general public, those with the &#128216 prefix are mostly personal notes.</p>
-<!--<p>I built this blog myself starting from a Rust script, which converts LaTeX to blog posts and organizes the webpage's directory. To learn more about my blog read <a href="/comp_posts/0)--How-I-built-this-blog/post.html">this post</a>.</p>
-<p>The content on this website is licensed under <a href="https://creativecommons.org/licenses/by/4.0/">Creative Commons Attribution 4.0 International License</a>.</p>-->
+<p>Hi I'm Tommaso, I am a developer building stuff on the Stellar Network at <a target="_blank" href="https://github.com/xycloo/">Xycloo Labs</a>, and 
+currently part-time <a href="https://stellar.org/foundation">Stellar Development Foundation</a> contractor helping to build Soroban-related educational contract-based games 
+(<a href="https://fcaooc.com/">fcaooc</a>, <a href="https://quest.stellar.org/">stellar quest</a>, <a href="https://rpciege.com/">rpciege</a>). 
+<br/><br/>
+My focus lies in decentralized ledger technology, smart contracts, Decentralized Finance, and SaaS. My preferred language is Rust,
+but I'm proficient with Javascript, Python, and I'm learning Zig (which is now becoming my second preferred lang).
+<br/><br/>
+Articles with the &#128215 prefix are intended to be read by a general public, those with the &#128216 prefix are mostly personal notes.</p>
+
+<p>The content on this website is licensed under <a href="https://creativecommons.org/licenses/by/4.0/">Creative Commons Attribution 4.0 International License</a>.</p>
 </div>
+<br/>
 <div id="icons">
     <ul>
-        <li><a target="_blank" href="https://twitter.com/heytdep"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-twitter" viewBox="0 0 16 16"> <path d="M5.026 15c6.038 0 9.341-5.003 9.341-9.334 0-.14 0-.282-.006-.422A6.685 6.685 0 0 0 16 3.542a6.658 6.658 0 0 1-1.889.518 3.301 3.301 0 0 0 1.447-1.817 6.533 6.533 0 0 1-2.087.793A3.286 3.286 0 0 0 7.875 6.03a9.325 9.325 0 0 1-6.767-3.429 3.289 3.289 0 0 0 1.018 4.382A3.323 3.323 0 0 1 .64 6.575v.045a3.288 3.288 0 0 0 2.632 3.218 3.203 3.203 0 0 1-.865.115 3.23 3.23 0 0 1-.614-.057 3.283 3.283 0 0 0 3.067 2.277A6.588 6.588 0 0 1 .78 13.58a6.32 6.32 0 0 1-.78-.045A9.344 9.344 0 0 0 5.026 15z"/> </svg> </a></li>
-        <li><a target="_blank" href="https://tdep.medium.com/"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-medium" viewBox="0 0 16 16"> <path d="M9.025 8c0 2.485-2.02 4.5-4.513 4.5A4.506 4.506 0 0 1 0 8c0-2.486 2.02-4.5 4.512-4.5A4.506 4.506 0 0 1 9.025 8zm4.95 0c0 2.34-1.01 4.236-2.256 4.236-1.246 0-2.256-1.897-2.256-4.236 0-2.34 1.01-4.236 2.256-4.236 1.246 0 2.256 1.897 2.256 4.236zM16 8c0 2.096-.355 3.795-.794 3.795-.438 0-.793-1.7-.793-3.795 0-2.096.355-3.795.794-3.795.438 0 .793 1.699.793 3.795z"/> </svg></a></li>
-        <li><a target="_blank" href="https://tdep.xycloo.com/"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-house-door-fill" viewBox="0 0 16 16"> <path d="M6.5 14.5v-3.505c0-.245.25-.495.5-.495h2c.25 0 .5.25.5.5v3.5a.5.5 0 0 0 .5.5h4a.5.5 0 0 0 .5-.5v-7a.5.5 0 0 0-.146-.354L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293L8.354 1.146a.5.5 0 0 0-.708 0l-6 6A.5.5 0 0 0 1.5 7.5v7a.5.5 0 0 0 .5.5h4a.5.5 0 0 0 .5-.5z"/> </svg></a></li>
+        <li><a target="_blank" href="https://github.com/heytdep"><svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 16 16">
+        <path
+          d="M8 .188c-4.418 0-8 3.582-8 8 0 3.535 2.291 6.527 5.471 7.594.4.074.547-.174.547-.384 0-.191-.007-.876-.012-1.589-2.227.485-2.695-1.061-2.695-1.061-.363-.923-.886-1.169-.886-1.169-.724-.495.055-.486.055-.486.802.057 1.223.824 1.223.824.712 1.218 1.87.867 2.326.663.072-.516.277-.866.503-1.064-1.763-.2-3.617-.881-3.617-3.927 0-.867.31-1.578.824-2.135-.083-.201-.358-1.009.078-2.101 0 0 .666-.213 2.185.815.635-.177 1.313-.266 1.986-.268.672.002 1.35.091 1.985.268 1.52-1.028 2.184-.815 2.184-.815.438 1.092.162 1.9.08 2.101.515.557.823 1.268.823 2.135 0 3.053-1.857 3.724-3.623 3.922.285.246.54.731.54 1.474 0 1.065-.01 1.923-.01 2.191 0 .213.144.463.55.383 3.179-1.067 5.468-4.059 5.468-7.594 0-4.418-3.582-8-8-8z"
+        />
+      </svg>
+      </a></li>
     </ul>
 </div>
-<!--<hr/>-->
 </div>
 <div class="articles"><ul>"#,
     );
+    
     ul.push_str(&li_list);
     ul.push_str("</ul></div></div></body>");
 
@@ -240,14 +332,16 @@ fn write_index() {
     let posts_dirs = get_comp();
 
     let posts = posts_dirs
+        .iter()
         .filter_map(|entry| {
-            entry.ok().and_then(|e| {
-                e.path()
+            
+                entry.as_path()
                     .file_name()
                     .and_then(|n| n.to_str().map(|s| String::from(s)))
-            })
+            
         })
         .collect::<Vec<String>>();
+    
     let content = gen_index_content(posts);
     fs::write("./index.html", content);
     println!("[+] Successfully built posts");
@@ -256,17 +350,20 @@ fn write_index() {
 fn main() {
     preemptive_remove();
     let drafts = get_drafts();
+    
     let posts = drafts
+        .iter()
         .filter_map(|entry| {
-            entry.ok().and_then(|e| {
-                e.path()
+            
+                entry.as_path()
                     .file_name()
                     .and_then(|n| n.to_str().map(|s| String::from(s)))
-            })
+            
         })
         .collect::<Vec<String>>();
 
     for draft in posts {
+        println!("{:?}", draft);
         to_html_pd(draft);
     }
 
