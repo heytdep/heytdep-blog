de86ba8187c44c6723fc251699c701d701b838d2
heytdep <tommaso@xycloo.com>
2024-11-05 21:32:10 +0700

    first one step closer to defi

---
+++ target/debug/deps/publish-cefc685dbfb73aff.d
@@ -0,0 +1,5 @@
+/Users/tdep/projects/heytdep.github.io/target/debug/deps/libpublish-cefc685dbfb73aff.rmeta: src/bin/publish.rs
+
+/Users/tdep/projects/heytdep.github.io/target/debug/deps/publish-cefc685dbfb73aff.d: src/bin/publish.rs
+
+src/bin/publish.rs:
